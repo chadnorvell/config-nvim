863f39cea12c6359974e5829ebfb4be337f4e775
Chad Norvell <chadxnor@gmail.com>
2025-04-25 16:05:29 -0700

    Enable completion via tab

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -8,6 +8,21 @@ return {
         local cmp = require("cmp")
 
         cmp.setup({
+            mapping = {
+                ["<Tab>"] = cmp.mapping(function(fallback)
+                    -- Confirm with tab, and if no entry is selected, confirm the first item
+                    if cmp.visible() then
+                        local entry = cmp.get_selected_entry()
+                        if not entry then
+                            cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+                        end
+                        cmp.confirm()
+                    else
+                        fallback()
+                    end
+                end, { "i", "s", "c" }),
+            },
+
             snippet = {
                 expand = function(args)
                     vim.snippet.expand(args.body)
