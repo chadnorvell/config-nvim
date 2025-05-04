76a502e4a20de69fd8b5e0d77249a7d98773eb00
Chad Norvell <chadxnor@gmail.com>
2025-05-03 19:05:34 -0700

    Format on save

--- lua/plugins/format.lua
+++ lua/plugins/format.lua
@@ -1,6 +1,7 @@
 return {
     "stevearc/conform.nvim",
     opts = {
+        format_on_save = { timeout_ms = 500 },
         formatters_by_ft = {
             lua = { "stylua" },
             python = { "ruff" },
